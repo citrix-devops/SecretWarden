b4f625f35c53fd50bb89dbc2e59553851d25c9c7
Cyanoth <32278428+Cyanoth@users.noreply.github.com>
2019-05-16 20:08:04 +0100

    Update ScanIncompleteException to correctly throw custom exception

--- src/main/java/com/cyanoth/secretwarden/ScanIncompleteException.java
+++ src/main/java/com/cyanoth/secretwarden/ScanIncompleteException.java
@@ -1,9 +1,9 @@
 package com.cyanoth.secretwarden;
 
-public class ScanIncompleteException extends Exception {
+class ScanIncompleteException extends Exception {
 
-   public String toString() {
-      return "Attempted to retrieve secret scan results, but a scan has not been started yet!";
+   ScanIncompleteException() {
+      super("Attempted to retrieve secret scan results, but a scan has not been started yet!");
    }
 
 }
