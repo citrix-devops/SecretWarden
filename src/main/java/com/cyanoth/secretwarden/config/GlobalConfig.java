0e170db76eb5daaebf6b8a8740b81c9ff0ee641c
Cyanoth <32278428+Cyanoth@users.noreply.github.com>
2019-05-20 18:43:13 +0100

    Prepare for global config page + settings

--- src/main/java/com/cyanoth/secretwarden/config/GlobalConfig.java
+++ src/main/java/com/cyanoth/secretwarden/config/GlobalConfig.java
@@ -1,4 +1,35 @@
 package com.cyanoth.secretwarden.config;
 
-public class GlobalConfig {
+import com.atlassian.bitbucket.user.SecurityService;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.sal.api.pluginsettings.PluginSettings;
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Scanned
+public class GlobalConfig  {
+    private static final String PLUGIN_KEY = "com.cyanoth.secretwarden"; // This need to remain hardcoded incase of changes to the plugin name (keep settings)
+    private static final Logger log = LoggerFactory.getLogger(GlobalConfig.class);
+
+    private final PluginSettingsFactory pluginSettingsFactory;
+    private final SecurityService securityService;
+    private final Permiss
+
+
+
+    //EVENT PUBLISHER?
+    public GlobalConfig(PluginSettingsFactory pluginSettingsFactory,
+                                     SecurityService securityService,
+
+                                       PermissionValidationService validationService) {
+        this.eventPublisher = eventPublisher;
+        this.pullRequestService = pullRequestService;
+        this.securityService = securityService;
+        this.transactionTemplate = transactionTemplate;
+        this.validationService = validationService;
+
+        pluginSettings = pluginSettingsFactory.createSettingsForKey(PLUGIN_KEY);
+    }
+
 }
