04f4ff7c4f62d762d6e185afab96afd52e9bd3b3
Cyanoth <32278428+Cyanoth@users.noreply.github.com>
2019-06-04 09:23:32 +0100

    Mark SecretScanner scan method as nullable incase of not-yet-implemented scanners

--- src/main/java/com/cyanoth/secretwarden/SecretScanner.java
+++ src/main/java/com/cyanoth/secretwarden/SecretScanner.java
@@ -1,10 +1,13 @@
 package com.cyanoth.secretwarden;
 
+import javax.annotation.Nullable;
+
 /**
  * Interface which all secret scan varieties must implement (pull request scan, repository scan, branch scan etc.)
  */
 public interface SecretScanner {
 
+    @Nullable
     SecretScanResult scan(Boolean force) throws SecretScanException;
 
 }
